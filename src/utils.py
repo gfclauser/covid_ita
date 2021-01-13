ab6cdf8081038a0409914e528fc1e146f3b2f7fb
Giorgio Francesco Clauser <clauser.giorgio@gmail.com>
2021-01-13 10:35:51 +0100

    more test

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def plot_time_lines(ax, zz):
 
 
 def directory_cleanup(region):
-    """Remove old plots andd create directory for new ones."""
+    """Remove old plots and create directory for new ones."""
     dir_path = f"/Users/giorgioclauser/Documents/covid_plts/{region}"
     shutil.rmtree(dir_path)
     os.mkdir(dir_path)
